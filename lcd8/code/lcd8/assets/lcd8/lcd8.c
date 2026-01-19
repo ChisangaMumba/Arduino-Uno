2a190400e23bc0f44e494899478e7f831575454c
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-01-19 12:34:43 +0200

    Add files via upload

---
+++ lcd8/code/lcd8/assets/lcd8/lcd8.c
@@ -0,0 +1,86 @@
+#ifndef LCD8_C
+#define	LCD8_C
+//------------------------------------------------------------------------------
+#include "lcd8.h"
+//-----------------------Follow Datasheet instructions--------------------------
+void LCD_init()
+{
+  pinMode(RS, OUTPUT);
+  pinMode(EN, OUTPUT);
+
+  pinMode(D0, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(D7, OUTPUT);
+
+  LCD_command(home);
+  LCD_command(function_set8);   
+  LCD_command(display_on);      
+  LCD_command(auto_increment);
+}
+//------------------------------------------------------------------------------
+void LCD_command(uint8_t command)
+{
+  digitalWrite(RS, LOW);          //  RS = 0;     //  Instruction register ( commands ).
+  LCD_transmit(command);
+}
+//------------------------------------------------------------------------------
+void LCD_data(uint8_t data)
+{
+  digitalWrite(RS, HIGH);         //  RS = 1;     //  Data register  ( strings and characters ).
+  LCD_transmit(data);
+}
+//------------------------------------------------------------------------------
+void LCD_string(uint8_t row, uint8_t column, uint8_t text[])
+{
+  LCD_command(rowAddr[row] + column);  // Set coordinates.
+
+  for(uint8_t i = 0; text[i]; i++)
+  {
+    LCD_data(text[i]);
+  }
+}
+//------------------------------------------------------------------------------
+void LCD_transmit(uint8_t data)
+{
+  uint8_t pinBits[8];
+   
+  LCD_getBits(pinBits, data);
+  LCD_sendBits(pinBits);
+}
+//------------------------------------------------------------------------------
+void LCD_sendBits(uint8_t pinBit[])
+{
+  digitalWrite(D0, pinBit[0]);   //  D0 = pinBit[0];
+  digitalWrite(D1, pinBit[1]);   //  D1 = pinBit[1];
+  digitalWrite(D2, pinBit[2]);   //  D2 = pinBit[2];
+  digitalWrite(D3, pinBit[3]);   //  D3 = pinBit[3];
+
+  digitalWrite(D4, pinBit[4]);   //  D4 = pinBit[4];
+  digitalWrite(D5, pinBit[5]);   //  D5 = pinBit[5];
+  digitalWrite(D6, pinBit[6]);   //  D6 = pinBit[6];
+  digitalWrite(D7, pinBit[7]);   //  D7 = pinBit[7];
+  
+  latch();
+}
+//------------------------------------------------------------------------------
+void LCD_getBits(uint8_t pinBit[], uint8_t data)
+{ 
+  for(uint8_t i = 0; i < 8; i++)
+  {
+    pinBit[i] = ((data >> i) & 0x01) ? HIGH : LOW;
+  }
+}
+//------------------------------------------------------------------------------
+void latch()
+{
+  digitalWrite(EN, HIGH);           //  EN  = 1;
+  delay(5);
+  digitalWrite(EN, LOW);            //  EN = 0;
+}
+//------------------------------------------------------------------------------
+#endif
