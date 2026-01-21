40a94e31bc05db15e543bb144b5573c4aabe99bc
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-01-21 15:57:45 +0200

    Add files via upload

---
+++ SHT75_Oled/code/SHT75/SHT75.ino
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------
+#include "assets/sht7x/sht7x.c"
+#include "assets/ssd1306/ssd1306.c"
+//------------------------------------------------------------------------------
+uint8_t str[65], numStr[12];
+float tempr = 0;
+//------------------------------------------------------------------------------
+void setup() 
+{
+  SHT7x_init();
+  
+  ssd1306_init();
+  ssd1306_print(0, 30, "SHT75 sensor"); 
+  ssd1306_print(2,  0, "Readings:");
+}
+//------------------------------------------------------------------------------
+void loop()
+{
+  tempr = SHT7x_getTemp();
+
+  dtostrf(SHT7x_getHumd(tempr), 6, 2, numStr);     // 6 chars min total width, 2 after decimal point
+  sprintf(str, "Humd: %s%c", numStr, '%');
+  ssd1306_print(4, 0, str);
+
+  dtostrf(tempr, 6, 2, numStr);   
+  sprintf(str, "Temp: %s°C", numStr);
+  ssd1306_print(5, 0, str);
+
+  delay(100);
+}
+//------------------------------------------------------------------------------
