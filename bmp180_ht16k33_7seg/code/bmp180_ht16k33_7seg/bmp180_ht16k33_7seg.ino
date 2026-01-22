7b0d5be16993093f5ad1ab3966e4f4c0416df4bb
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-01-22 15:57:38 +0200

    Add files via upload

---
+++ bmp180_ht16k33_7seg/code/bmp180_ht16k33_7seg/bmp180_ht16k33_7seg.ino
@@ -0,0 +1,27 @@
+//-----------------------------------------------------------------------------------
+#include "assets/bmp180/bmp180.c"
+#include "assets/ht16k33/ht16k33.c"
+//-----------------------------------------------------------------------------------
+float press, temp;
+uint8_t str[15];
+//-----------------------------------------------------------------------------------
+void setup(void)
+{
+  ht16k33_init(15);   // Initialize HT16K33 with brightness level 15 
+  bmp180_init();
+}
+//-----------------------------------------------------------------------------------
+void loop(void) 
+{ 
+  temp = bmp180_getTemp();   // always read temperature first, needed for pressure calculation
+  // press = bmp180_getPress();   // Uncomment this line if you want to read pressure
+
+  // dtostrf(press, 6, 1, str);   // 6 chars min total width, 1 after decimal point
+  // strcat(str, "hPA");
+  // ht16k33_displayText(str);
+  
+  dtostrf(temp, 7, 1, str);   // 7 chars min total width, 1 after decimal point
+  strcat(str, "°C");
+  ht16k33_displayText(str);
+}
+//-----------------------------------------------------------------------------------
