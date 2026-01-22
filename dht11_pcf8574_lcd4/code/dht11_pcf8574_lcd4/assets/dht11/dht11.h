be231e098472c0f0bdb89191e5bd19a59f5afb84
Chisanga <97330041+ChisangaMumba@users.noreply.github.com>
2026-01-22 16:11:46 +0200

    Add files via upload

---
+++ dht11_pcf8574_lcd4/code/dht11_pcf8574_lcd4/assets/dht11/dht11.h
@@ -0,0 +1,21 @@
+#ifndef DHT11_H
+#define	DHT11_H
+//------------------------------------------------------------------------------
+uint8_t DATA_PIN = 10; // Pin connected to DHT11 data pin
+//------------------------------------------------------------------------------
+typedef struct 
+{
+    int16_t  temperature;
+	int16_t  humidity;
+    uint8_t  checksum;        
+} dht11_t;
+//------------------------------------------------------------------------------
+void 	oneWire_start(void);			
+uint8_t oneWire_getResponse(void);			
+uint8_t oneWire_read8(void);		
+uint8_t dht11_calcChecksum(uint8_t sensorData[]);
+uint8_t dht11_isDataValid(uint8_t sensorData[]);
+uint8_t dht11_getSensorData(uint8_t sensorData[]);
+void 	dht11_setFields(dht11_t *sensor, uint8_t data[]);
+//------------------------------------------------------------------------------
+#endif
---
+++ dht11_pcf8574_lcd4/code/dht11_pcf8574_lcd4/dht11_pcf8574_lcd4.ino
@@ -0,0 +1,32 @@
+//-----------------------------------------------------------------------------------
+#include "assets/dht11/dht11.c"
+#include "assets/lcd/lcd_serial.c"
+//-----------------------------------------------------------------------------------
+uint8_t str[18], sensorData[5];
+dht11_t sensor;
+//-----------------------------------------------------------------------------------
+void setup(void)
+{
+  lcd_init();
+}
+//-----------------------------------------------------------------------------------
+void loop(void) 
+{
+  uint8_t ok = dht11_getSensorData(sensorData);
+  
+  if(ok == 1 && dht11_isDataValid(sensorData) == 1)
+  {
+    dht11_setFields(&sensor, sensorData);
+      
+    sprintf(str, "Humd:%3i%c", sensor.humidity, '%');
+    lcd_string(0, 0, str);
+    
+    sprintf(str, "Temp:%3i°C", sensor.temperature);
+    lcd_string(1, 0, str);
+  }
+  else
+      {
+        lcd_string(0, 0, "Error");
+      }
+}
+//-----------------------------------------------------------------------------------
